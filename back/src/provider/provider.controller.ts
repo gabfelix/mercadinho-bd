3d953bfb97bec6f4b1ba5435e1a5587394b275be
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-09-05 17:05:58 -0300

    back: Provider CRUD

--- back/src/provider/provider.controller.ts
+++ back/src/provider/provider.controller.ts
@@ -1,9 +1,28 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ProviderService } from './provider.service';
 
 @Controller('provider')
 export class ProviderController {
   constructor(private readonly providerService: ProviderService) {}
+
+  @Get()
+  findAll() {
+    return this.providerService.many({});
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.providerService.one(id);
+  }
 
   @Post()
   async create(
@@ -11,5 +30,20 @@ export class ProviderController {
     { cnpj, contactId }: { name: string; cnpj: string; contactId: number },
   ) {
     return this.providerService.create(cnpj, contactId);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body()
+    { cnpj, contactId }: { cnpj: string; contactId: number },
+  ) {
+    if (!cnpj && !contactId) return this.providerService.one(id);
+    return this.providerService.update(id, { cnpj, contactId });
+  }
+
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    return this.providerService.delete(id);
   }
 }
