f103b6284efd6aef34461f481a478687f54b0854
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-09-05 15:34:14 -0300

    back: Add contact migration & boilerplate

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma.service';
+import { ContactService } from './contact/contact.service';
+import { ContactController } from './contact/contact.controller';
+import { ContactModule } from './contact/contact.module';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [UserModule, ContactModule],
+  controllers: [AppController, ContactController],
+  providers: [AppService, PrismaService, ContactService],
 })
 export class AppModule {}
