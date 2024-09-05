5397c264310f2ab54dc06b5919c9c262e7cce936
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-09-05 16:49:46 -0300

    back: Provider creation

--- back/src/provider/provider.controller.ts
+++ back/src/provider/provider.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ProviderService } from './provider.service';
 
 @Controller('provider')
 export class ProviderController {
   constructor(private readonly providerService: ProviderService) {}
+
+  @Post()
+  async create(
+    @Body()
+    { cnpj, contactId }: { name: string; cnpj: string; contactId: number },
+  ) {
+    return this.providerService.create(cnpj, contactId);
+  }
 }
--- back/src/provider/provider.service.ts
+++ back/src/provider/provider.service.ts
@@ -1,7 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Prisma, Provider } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class ProviderService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async create(cnpj: string, contactId: number): Promise<Provider> {
+    const data: Prisma.ProviderCreateInput = {
+      cnpj,
+      contact: { connect: { id: contactId } },
+    };
+    const createdProvider = await this.prismaService.provider
+      .create({ data })
+      .catch((e) => {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+          if (e.code === 'P2002') {
+            const target = e.meta?.target?.[0] ?? '';
+            const field = target[0].toUpperCase() + target.slice(1);
+            throw new BadRequestException(`${field} já está em uso`);
+          }
+        }
+        if (e instanceof Prisma.PrismaClientValidationError) {
+          throw new BadRequestException('Erro de validação');
+        }
+      });
+    if (!createdProvider)
+      throw new BadRequestException('Erro de criação de fornecedor');
+    return createdProvider;
+  }
 }
